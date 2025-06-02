01a2ffea1cf4751d5d02b6f2896292ae529e744d
Huy Nguyen <nguye2cy@mail.uc.edu>
2025-06-01 21:49:24 -0400

    Add contents for sections

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,14 @@ export default function About() {
   return (
     <div className="max-w-2xl mx-auto py-10">
       <h1 className="text-3xl font-bold mb-4">About Us</h1>
-      <p>We're building TruFlo to help you unlock your productivity potential!</p>
+      <p>We are trying to be the first productivity app that understands your mood to help  you break free from distractions and unproductive habits</p>
+      <p>Meet the Team:</p>
+      <ul className="list-disc pl-5">
+        <li><strong>Sourish Kumar</strong> - Founder & CEO</li>
+        <li><strong>Freddie Letzer</strong> - Co-Founder & CFO</li>
+        <li><strong>Leo Nguyen</strong> - CTO</li>
+        <li><strong>David Nguyen</strong> - Tech Director</li>
+      </ul>
     </div>
   );
 }
